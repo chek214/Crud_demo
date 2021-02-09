6d06222e2174ac64c903a418973582b1786897b8
Chek <chek214@live.com>
2021-02-09 13:25:20 -0600

    App view vuejs

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,11 @@
-require('./bootstrap');
+require('./bootstrap')
+
+import Vue from 'vue'
+import App from './Views/App.vue'
+
+const app = new Vue({
+    el: '#app',
+    render: h => h(App)
+})
+
+export default app
