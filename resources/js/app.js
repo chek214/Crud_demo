bc3634247d2ac560c2ea6848feed8b8d4d8092d7
Chek <chek214@live.com>
2021-02-09 16:29:45 -0600

    bootstrap 5 added

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,6 +3,8 @@ require('./bootstrap')
 import Vue from 'vue'
 import App from './Views/App.vue'
 
+import 'bootstrap/dist/css/bootstrap.min.css'
+
 const app = new Vue({
     el: '#app',
     render: h => h(App)
